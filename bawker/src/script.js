db7dbc4c957815343c7ab95f8e253530c31e1176
Pink-Hat-Hacker <zyv@udel.edu>
2022-10-03 19:11:21 -0400

    jquery hahaha

--- bawker/src/script.js
+++ bawker/src/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     //Character limit js
-    var maxLength= 140;
+    var maxLength = 140;
     $("textarea").keypress(function(){
        var length = $(this).val().length;
-       var length = maxLength-length;
+       var length = maxLength - length;
        $("#countdown").text(length);
     })
 });
